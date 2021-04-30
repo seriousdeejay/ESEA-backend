762bd0902d6ec8b4f0f1462915830262c4e6a36f
Your Name <your@email.com>
2021-04-30 16:44:50 +0200

    Implemented database connection V2

--- core/models/survey_response.py
+++ core/models/survey_response.py
@@ -26,10 +26,10 @@ class SurveyResponse(models.Model):
     objects = SurveyResponseManager()
     # id = models.BigIntegerField(primary_key = True)
     # id = models.AutoField(primary_key=True)
-    #esea_account = models.ForeignKey('EseaAccount', related_name="responses", on_delete=models.CASCADE, null=True)
-    #survey = models.ForeignKey('Survey', related_name="responses", on_delete=models.CASCADE)
-    #respondent = models.OneToOneField('Respondent', related_name="response", on_delete=models.CASCADE) # , primary_key=True
-    #token = models.CharField(max_length=8)
+    esea_account = models.ForeignKey('EseaAccount', related_name="responses", on_delete=models.CASCADE, null=True)
+    survey = models.ForeignKey('Survey', related_name="responses", on_delete=models.CASCADE)
+    respondent = models.OneToOneField('Respondent', related_name="response", on_delete=models.CASCADE) # , primary_key=True, null=True for now!
+    token = models.CharField(max_length=8)
     finished = models.BooleanField(default=False)
 
     class Meta:
